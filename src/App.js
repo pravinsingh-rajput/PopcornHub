d44cf7c9142feedc829fbbc07a0a7191041fce87
pravinsingh-rajput <singhpravin17a2@gmail.com>
2023-04-25 12:07:28 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  //   useEffect(() => {
+  //     fetchdata();
+  //   }, []);
+
+  //   const fetchdata = async () => {
+  //     const data = await axios
+  //       .get(
+  //         `
+  // https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_TOKEN}`
+  //       )
+  //       .then((res) => {
+  //         console.log(res);
+  //       });
+  //   };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Day 1 Building a Movie Webiste</h3>
+      <h1>PopcornHub</h1>
+      <a href="pravinsingh.me">pravinsingh.me</a>
     </div>
   );
 }
