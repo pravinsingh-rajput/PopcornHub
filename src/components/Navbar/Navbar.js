725f1632bcc6a723a11eace313d0605f209ce5ed
pravinsingh-rajput <singhpravin17a2@gmail.com>
2023-05-01 10:50:10 +0530

    Navbar & Hero Section

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import SearchIcon from "@mui/icons-material/Search";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/CloseOutlined";
 import { NavLink } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const [mobile, setMobile] = useState(false);
 
   const homenavigate = () => {
     navigate("/");
@@ -13,12 +17,18 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar_container">
-        <h3 onClick={homenavigate} className="navtitle">
+        <h2 onClick={homenavigate} className="navtitle">
           PopcornHub
-        </h3>
+        </h2>
         <nav>
-          <NavLink to="/movie">Movie</NavLink>
-          <NavLink to="/tvshow">Tv Show</NavLink>
+          <div className="desktop_menu">
+            <NavLink to="/movie">Movie</NavLink>
+            <NavLink to="/tvshow">Tv Show</NavLink>
+          </div>
+          <SearchIcon style={{ cursor: "pointer" }} />
+          <div className="hamburger_menu">
+            <MenuIcon style={{ cursor: "pointer" }} />
+          </div>
         </nav>
       </div>
     </div>
