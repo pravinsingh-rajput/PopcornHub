149aeb7ad3f3c8e2fef26410106d890167471cd4
pravinsingh-rajput <singhpravin17a2@gmail.com>
2023-05-01 12:39:47 +0530

    Testing bg image

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,12 +2,11 @@ import React, { useEffect, useState } from "react";
 import useFetch from "../../hooks/useFetch";
 import { useSelector } from "react-redux";
 import "./Hero.css";
-import LazyImage from "../LazyLoad/LazyLoadImage";
 
 const Hero = () => {
   const [bgimg, setBgimg] = useState("");
   const { url } = useSelector((state) => state.home);
-  const { data, loading } = useFetch("/movie/upcoming");
+  const { data } = useFetch("/movie/upcoming");
 
   const [search, setSearch] = useState("");
 
@@ -30,32 +29,30 @@ const Hero = () => {
     <div
       className="hero_section"
       style={{
-        backgroundImage: `url(${bgimg})`,
+        backgroundImage: `url(${bgimg}) `,
         backgroundSize: "cover",
         backgroundPosition: "center center",
       }}
     >
-      <div className="hero_Content">
-        <h1 className="title">Welcome</h1>
-        <span className="title_description">
-          Explore a wide range of movies and TV shows on our website and watch
-          your favorite titles anytime, anywhere, on any device.
-        </span>
+      <h1 className="title">Welcome</h1>
+      <span className="title_description">
+        Explore a wide range of movies and TV shows on our website and watch
+        your favorite titles anytime, anywhere, on any device.
+      </span>
 
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={search}
-            onChange={inputHandler}
-            placeholder="Search Movies & TvShows ....."
-          />
-          <button onClick={handleSubmit} className="home_search">
-            Search
-          </button>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={search}
+          onChange={inputHandler}
+          placeholder="Search Movies & TvShows ....."
+        />
+        <button onClick={handleSubmit} className="home_search">
+          Search
+        </button>
+      </form>
 
-        <div className="opacity_layer"></div>
-      </div>
+      <div className="opacity_layer"></div>
     </div>
   );
 };
